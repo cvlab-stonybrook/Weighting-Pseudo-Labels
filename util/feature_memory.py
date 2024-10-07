430526dd43f3a5cd6eab0ca83131d22660e15326
prantikhowlader <51222390+prantikhowlader@users.noreply.github.com>
2024-10-07 10:34:44 -0400

    Add files via upload

---
+++ util/feature_memory.py
@@ -0,0 +1,65 @@
+""""
+Implementation for the Memory Bank for pixel-level feature vectors
+"""
+
+import torch
+import numpy as np
+import random
+
+class FeatureMemory:
+    def __init__(self, num_samples, dataset,  memory_per_class=2048, feature_size=256, n_classes=19):
+        self.num_samples = num_samples#10490
+        self.memory_per_class = memory_per_class#256
+        self.feature_size = feature_size#256
+        self.memory = [None] * n_classes
+        self.n_classes = n_classes#21
+        if dataset == 'cityscapes': # usually all classes in one image
+            self.per_class_samples_per_image = max(1, int(round(memory_per_class / num_samples)))
+        elif dataset == 'pascal': # usually only around 3 classes on each image, except background class
+            self.per_class_samples_per_image = max(1, int(n_classes / 3 * round(memory_per_class / num_samples)))#1
+
+    def add_features_from_sample_learned(self, model, features, class_labels, batch_size):
+        """
+        Updates the memory bank with some quality feature vectors per class
+        Args:
+            model: segmentation model containing the self-attention modules (contrastive_class_selectors)
+            features: BxFxWxH feature maps containing the feature vectors for the contrastive (already applied the projection head)
+            class_labels:   BxWxH  corresponding labels to the [features]
+            batch_size: batch size
+
+        Returns:
+
+        """
+        features = features.detach()
+        class_labels = class_labels.detach().cpu().numpy()
+        elements_per_class = batch_size * self.per_class_samples_per_image
+
+        # for each class, save [elements_per_class]
+        for c in range(self.n_classes):
+            mask_c = class_labels == c  # get mask for class c
+            #selector = model.module.__getattr__('contrastive_class_selector_' + str(c))  # get the self attention moduel for class c
+            features_c = features[mask_c, :] # get features from class c#[3665, 256]
+            if features_c.shape[0] > 0:
+                #print(features_c.shape[0])
+                if features_c.shape[0] > elements_per_class:
+                    with torch.no_grad():
+#                         # get ranking scores
+#                         rank = selector(features_c)
+#                         rank = torch.sigmoid(rank)
+#                         # sort them
+#                         _, indices = torch.sort(rank[:, 0], dim=0)
+#                         indices = indices.cpu().numpy()
+                        features_c = features_c.cpu().numpy()
+                        indices = random.sample(list(range(features_c.shape[0])),elements_per_class)
+                        # get features with highest rankings
+                        features_c = features_c[indices, :]
+                        new_features = features_c[:elements_per_class, :]
+                else:
+                    new_features = features_c.cpu().numpy()
+
+                if self.memory[c] is None: # was empy, first elements
+                    self.memory[c] = new_features
+
+                else: # add elements to already existing list
+                    # keep only most recent memory_per_class samples
+                    self.memory[c] = np.concatenate((new_features, self.memory[c]), axis = 0)[:self.memory_per_class, :]
